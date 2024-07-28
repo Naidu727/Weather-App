db0d541550ac74dd3c80145574a5904dd15c2649
Naidu727 <130582105+Naidu727@users.noreply.github.com>
2024-07-28 20:43:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,75 @@
-const inputBox = document.querySelector('.input-box');
-const searchBtn = document.getElementById('searchBtn');
-const weather_img = document.querySelector('.weather-img');
-const temperature = document.querySelector('.temperature');
-const description = document.querySelector('.description');
-const humidity = document.getElementById('humidity');
-const wind_speed = document.getElementById('wind-speed');
-
-const location_not_found = document.querySelector('.location-not-found');
-
-const weather_body = document.querySelector('.weather-body');
-
-
-async function checkWeather(city){
-    const api_key = "efd40d56c72f6bdece9f5fff5f058407";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-
-    const weather_data = await fetch(`${url}`).then(response => response.json());
-
-
-    if(weather_data.cod === `404`){
-        location_not_found.style.display = "flex";
-        weather_body.style.display = "none";
-        console.log("error");
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+const cityHide = document.querySelector('.city-hide');
+search.addEventListener('click', () => {
+    const APIKey = "efd40d56c72f6bdece9f5fff5f058407";
+    const city = document.querySelector('.search-box input').value;
+    
+    if (city.trim() === '') {
         return;
     }
-
-    console.log("run");
-    location_not_found.style.display = "none";
-    weather_body.style.display = "flex";
-    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
-    description.innerHTML = `${weather_data.weather[0].description}`;
-
-    humidity.innerHTML = `${weather_data.main.humidity}%`;
-    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
-
-
-    // switch(weather_data.weather[0].main){
-    //     case 'Clouds':
-    //         weather_img.src = "/assets/cloud.png";
-    //         break;
-    //     case 'Clear':
-    //         weather_img.src = "/assets/clear.png";
-    //         break;
-    //     case 'Rain':
-    //         weather_img.src = "/assets/rain.png";
-    //         break;
-    //     case 'Mist':
-    //         weather_img.src = "/assets/mist.png";
-    //         break;
-    //     case 'Snow':
-    //         weather_img.src = "/assets/snow.png";
-    //         break;
-
-    // }
-    switch(weather_data.weather[0].main){
-        case 'Clouds':
-            weather_img.src = "assets/cloud.png";
-            break;
-        case 'Clear':
-            weather_img.src = "assets/clear.png";
-            break;
-        case 'Rain':
-            weather_img.src = "assets/rain.png";
-            break;
-        case 'Mist':
-            weather_img.src = "assets/mist.png";
-            break;
-        case 'Snow':
-            weather_img.src = "assets/snow.png";
-            break;
-    }
     
-
-    console.log(weather_data);
-}
-
-
-searchBtn.addEventListener('click', ()=>{
-    checkWeather(inputBox.value);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`;
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            if (json.cod === "404") {
+                cityHide.textContent = city;
+                container.style.height = '400px';
+                weatherBox.classList.remove('active');
+                weatherDetails.classList.remove('active');
+                error404.classList.add('active');
+                return;
+            }
+            container.style.height = '555px';
+            weatherBox.classList.add('active');
+            weatherDetails.classList.add('active');
+            error404.classList.remove('active');
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+            if (cityHide.textContent == city) {
+                return;
+            } else {
+                cityHide.textContent = city;
+                container.style.height = '555px';
+                container.classList.add('active');
+                weatherBox.classList.add('active');
+                weatherDetails.classList.add('active');
+                error404.classList.remove('active');
+                switch (json.weather[0].main) {
+                    case 'Clear':
+                        image.src = 'images/clear.png';
+                        break;
+                    case 'Rain':
+                        image.src = 'images/rain.png';
+                        break;
+                    case 'Snow':
+                        image.src = 'images/snow.png';
+                        break;
+                    case 'Clouds':
+                        image.src = 'images/cloud.png';
+                        break;
+                    case 'Mist':
+                        image.src = 'images/mist.png';
+                        break;
+                    case 'Haze':
+                        image.src = 'images/cloud.png';
+                        break;
+                    default:
+                        image.src = 'images/cloud.png';
+                }
+                
+                temperature.innerHTML = `${Math.round(json.main.temp)}<span>°C</span>`;
+                description.innerHTML = `${json.weather[0].description}`;
+                humidity.innerHTML = `${json.main.humidity}%`;
+                wind.innerHTML = `${json.wind.speed} Km/h`;
+            }
+        });
 });
